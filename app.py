6d3c6540781dc150a5ffe7712c9feed03c53ef1b
Ubuntu <ubuntu@ip-172-31-89-132.ec2.internal>
2019-09-10 13:14:56 +0000

    Updated with insert task page

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World ... again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    _categories = mongo.db.categories.find()
+    category_list = [category for category in _categories]
+    return render_template('addtask.html', categories = category_list)
     
     
+    
+@app.route('/insert_task', methods= ['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+    
+    
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
+            
+            
+            
+           ## "mongodb+srv://root:r00tuser@myfirstcluster-h684p.mongodb.net/tasks?retryWrites=true&w=majority"
